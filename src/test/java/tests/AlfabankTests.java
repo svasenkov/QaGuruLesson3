2c8e677ba422e5dd7df8aa5f54d174d3039b9786
izuevro <56828102+izuevro@users.noreply.github.com>
2020-08-09 14:39:27 +0300

    Update test

---
+++ src/test/java/tests/AlfabankTests.java
@@ -0,0 +1,23 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class AlfabankTests {
+    static final String URL = "https://alfabank.ru/";
+
+    @Test
+    void archiveDepositListSize() {
+        open(URL); // Перейти на главную страницу
+        $(byTitle("Вклады")).click(); // Перейти на страницу Вклады
+        $$("[title=Инвестиции]").last().parent()
+                .preceding(2).click(); // Перейти на страницу Депозиты
+        $(".col-sm-frame p")
+                .sibling(0).$("a").click(); // Перейти на страницу Архивные депозиты
+        $(byClassName("product-cell__cell")).closest("div")
+                .$$(".product-cell__cell-back").shouldHaveSize(3); //Проверить отображение 3х депозитов
+    }
+}
